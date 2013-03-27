fce910acd2e5bc88e6a544a115d20c2c54f075d8
DJ Chung <djchung3@gmail.com>
2013-03-26 22:37:24 -0700

    first commit with hello world

---
+++ test/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by DJ Chung on 3/26/13.
+//  Copyright (c) 2013 DJ Chung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by DJ Chung on 3/26/13.
+//  Copyright (c) 2013 DJ Chung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+- (NSString *)helloWorld;
+
+@end
